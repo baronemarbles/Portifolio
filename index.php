3de64f872a5540c1540e217645a8383157645762
Marcio Barone <baronempbb@gmail.com>
2025-01-12 23:49:32 -0300

    Arquivos principal da landing page e arquivos de css e imagens

---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE HTML>
+
+<head lang = "pt-br">
+    <meta charset= "utf-8">
+    <link rel="stylesheet" href="style.css">
+    <title> Barone.marbles Resume</title>
+    
+</head>
+
+<body>
+<main>
+    <div id="header_container"><h1 id="header">Hi, there!</h1><h2 id="header">How yall doing?</h2></div>
+    <div id="sub_titulo_container"><h2 id ="sub_titulo"> here's my resume</h2></div>
+</main>
+
+<section> 
+    <img id="main_icon" src= "https://i.pinimg.com/236x/d8/06/2e/d8062e7d15d0b28dbc1aa40527b7117c.jpg">   
+
+</section>
+
+
+
+<div id="resume_container">
+
+    <div id="head_resume_container">
+        <img id="profile_icon" src= "https://i.pinimg.com/236x/d8/06/2e/d8062e7d15d0b28dbc1aa40527b7117c.jpg">  
+
+        <h2 id = 'name'>Marcio Paiva Barone Borges</h2>
+    </div>
+
+
+    <div id="cointainer_info_topics">
+        <div class="container_infos" id="container_education">
+            <h3> Education</h3>
+                <ul>
+                    <li><em>Liceu Santista Highschool 2005-2021</em></li>
+                    <li><strong>Computer Sciense Bachelor - Universidade Católica de Santos - 2022 - 2026</strong> </li>
+                </ul>
+        </div>
+        
+        
+        <div class="container_infos" id="container_languages">
+            <h3> Languages</h3>
+                <ul>
+                    <li>Portuguese - Native, Mother language</li>
+                    <li class = "bullet_b4_details">English - Advanced</li>
+                    <p class = "bullet_desc"> I had English as a subject since Kinderngarden up untill my HighSchool Senior Year graduation. These studies i carry on till today. Furthermore I attended to CNA Languague School in 2023, spent 1 year there and had the opportunity to take the Cambridge Lingua Skill test and got "C1 or above" in scoring. </p>
+
+                </ul>
+        </div>
+
+
+        <div class="container_infos" id="container_softskills">
+            <h3> Projects</h3>
+            <h6 id="h6_sub_titulo">Personal, Academic and intership</h6>
+                <ul>
+                    <li>Adaptability</li>
+                    <li>Accountability</li>
+                    <li>Empathy</li>
+                    <li>Active listening</li>
+                    <li>Emotional intelligence</li>
+                    <li>Resilience</li>
+                </ul>
+
+        </div>
+
+
+        <div class="container_infos" id="container_hardskills"> 
+            <h3> Hard Skills</h3>
+                <ul>
+                    <li>HTML, CSS Markup and Styling Languages</li>
+                    <li>C Programming language</li>
+                    <li>Python Programming language</li>
+                    <li>Java Programming language</li>
+                    <li>Adobe Photoshop</li>
+                    <li>Excel, Word, PowerPoint, Outlook</li>
+                    <li>Bilingual</li>
+                </ul>
+        </div>
+
+
+        
+    
+    </div>
+
+
+</div>
+
+</body>
+
+<footer></footer>
+
+
+
+</HTML>
